d2cfb4961c67e45a6341ae2c33b6ed85f6720bb5
Cori Kollin <44906595+corikollin@users.noreply.github.com>
2020-12-03 17:47:17 -0500

    artists in genre function

--- Final.py
+++ Final.py
@@ -125,5 +125,46 @@ def get_artists_in_genre(genre):
         i += 1
 
     return artists
+
+def get_artists_in_genre(genre):
+    url = "https://open.spotify.com"
+    genre_url = url + '/genre/' + genre
+    r = requests.get(genre_url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    results = soup.find_all('a')
+
+    for result in results:
+        if 'class' in result.attrs.keys() and result.attrs['class'] == ['cover', 'entity-type-playlist']:
+            r = requests.get(url + result.attrs['href'])
+            soup = BeautifulSoup(r.text, 'html.parser')
+            break
+
+    results = soup.find_all('script')
+
+    for result in results:
+        if result.contents != None and len(result.contents) > 0 and result.contents[0][:12] == '\n\t\t\t\tSpotify':
+            break
+    
+    start = 0
+
+    artists = {}
+    i = 0
+
+    while result.contents[0].find("artists", start) != -1 and i < 20:
+        start = result.contents[0].find("artists", start)
+        id_start = result.contents[0].find("id", start) + 5
+        id_end = result.contents[0].find(",", id_start) - 1
+        artist_id = result.contents[0][id_start:id_end]
+
+        name_start = result.contents[0].find("name", id_end) + 7
+        name_end = result.contents[0].find(",", name_start) - 1
+        artist_name = result.contents[0][name_start:name_end]
+
+        start = name_end
+        artists[artist_name] = artist_id
+        i += 1
+
+    return artists
   
   
