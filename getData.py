fa489cbcf514c477329d8b60475ec8a8b517d549
Cori Kollin <44906595+corikollin@users.noreply.github.com>
2020-12-06 21:18:40 -0500

    updated

--- getData.py
+++ getData.py
@@ -1,46 +1,114 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 import sqlite3
-import seaborn as sns
 import re
 import os
-import csv
-import numpy as np
-import matplotlib
-import pandas as pd
 
-def get_num_followers(artist):
+# returns a list of 10 of the top artists in the given genre
+def get_artists_in_genre(genre):
+    url = "https://open.spotify.com"
+    genre_url = url + '/genre/' + genre
+    r = requests.get(genre_url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    results = soup.find_all('a')
 
-    url = "https://www.google.com/search?q=instagram+followers+"
+    for result in results:
+        if 'class' in result.attrs.keys() and result.attrs['class'] == ['cover', 'entity-type-playlist']:
+            r = requests.get(url + result.attrs['href'])
+            soup = BeautifulSoup(r.text, 'html.parser')
+            break
+
+    results = soup.find_all('script')
+
+    for result in results:
+        if result.contents != None and len(result.contents) > 0 and result.contents[0][:12] == '\n\t\t\t\tSpotify':
+            break
+    
+    start = 0
+
+    artists = {}
+    i = 0
+
+    while result.contents[0].find("artists", start) != -1 and len(artists) < 10:
+        start = result.contents[0].find("artists", start)
+        id_start = result.contents[0].find("id", start) + 5
+        id_end = result.contents[0].find(",", id_start) - 1
+        artist_id = result.contents[0][id_start:id_end]
+
+        name_start = result.contents[0].find("name", id_end) + 7
+        name_end = result.contents[0].find(",", name_start) - 1
+        artist_name = result.contents[0][name_start:name_end]
+
+        start = name_end
+        artists[artist_name] = artist_id
+        i += 1
+
+    return artists
+
+# searches Google to find a given artist's Instagram username and returns this string
+def get_insta_username(artist):
+    url = "https://www.google.com/search?q=instagram+username+"
     artist = artist.lower()
-    url += artist.replace(' ','')
+    url += artist.replace(' ','+')
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     results = soup.find_all('div')
+    username = ''
 
     for result in results:
-        if 'class' in result.attrs.keys() and result.attrs['class'] == ['kCrYT']:
-            end = result.text.find("www.instagram.com ")
-            if end != -1:
-                result = result.next_sibling.next_sibling
-                break
+        if result.attrs == {} and result.text.find("Instagram photos") != -1:
+            start = result.text.find("@")
+            end = result.text.find(")",start)
+            if end == -1:
+                end = result.text.find("Instagram photos") - 3
+            username = result.text[start+1:end]
+            break
     
-    end = result.text.find("Followers")
-    if end != -1:
-        end -= 2
-        if result.text[end] >= '0' and result.text[end] <= '9':
-            return float(result.text[0:end + 1])
-        else:
-            num_followers = float(result.text[0:end])
-            if result.text[end] == 'm':
-                num_followers *= 1000000
-            elif result.text[end] == 'k':
-                num_followers *= 10000
-            return num_followers
+    if username == '':
+        url = "https://www.google.com/search?q=instagram+"
+        artist = artist.lower()
+        url += artist.replace(' ','')
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    return 0
+        results = soup.find_all('div')
 
+        for result in results:
+            if result.attrs == {} and result.text.find("Instagram photos") != -1:
+                start = result.text.find("@")
+                end = result.text.find(")",start)
+                if end == -1:
+                    end = result.text.find("Instagram photos") - 3
+                return result.text[start+1:end]
+    else:
+        return username
+
+    return ''
+
+# returns the number of Instagram followers based on the username given
+def get_num_followers(user):
+
+    base_url = "https://www.instagram.com/"
+    insta_username = user
+    ext = "/?__a=1"
+
+    url = base_url + insta_username + ext
+
+    try:
+        response = requests.get(url)
+        text = response.text
+        data = json.loads(text)
+
+        followers = data['graphql']['user']['edge_followed_by']['count']
+        
+        return followers
+    except:
+        return -1
+
+# returns the number of Monthly Spotify Streams of a given artist
 def get_monthly_listeners(artist, cur):
 
     cur.execute("SELECT artist, artist_id FROM Artists")
@@ -68,75 +136,3 @@ def get_monthly_listeners(artist, cur):
             break
 
     return monthly_listeners
-
-def get_artists_in_genre(genre):
-    url = "https://open.spotify.com"
-    genre_url = url + '/genre/' + genre
-    r = requests.get(genre_url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    
-    results = soup.find_all('a')
-
-    for result in results:
-        if 'class' in result.attrs.keys() and result.attrs['class'] == ['cover', 'entity-type-playlist']:
-            r = requests.get(url + result.attrs['href'])
-            soup = BeautifulSoup(r.text, 'html.parser')
-            break
-
-    results = soup.find_all('script')
-
-    for result in results:
-        if result.contents != None and len(result.contents) > 0 and result.contents[0][:12] == '\n\t\t\t\tSpotify':
-            break
-    
-    start = 0
-
-    artists = {}
-    i = 0
-
-    while result.contents[0].find("artists", start) != -1 and i < 19:
-        start = result.contents[0].find("artists", start)
-        id_start = result.contents[0].find("id", start) + 5
-        id_end = result.contents[0].find(",", id_start) - 1
-        artist_id = result.contents[0][id_start:id_end]
-
-        name_start = result.contents[0].find("name", id_end) + 7
-        name_end = result.contents[0].find(",", name_start) - 1
-        artist_name = result.contents[0][name_start:name_end]
-
-        start = name_end
-        artists[artist_name] = artist_id
-        i += 1
-
-    return artists
-
-def setUpIdTable(data, cur, conn):
-    for artist in data:
-        cur.execute("INSERT INTO Artists (artist_id,artist) VALUES (?,?)",(data[artist], artist))
-    conn.commit()
-
-def setUpGenreTable(genre, data, cur, conn):
-    for artist in data:
-        cur.execute("INSERT INTO Genres (genre,artist) VALUES (?,?)",(genre,artist))
-    conn.commit()
-
-def setUpInstagramTable(data, cur, conn):
-    for artist in data:
-        num_followers = len(artist)
-        #num_followers = get_num_followers(artist)
-        cur.execute("INSERT INTO Instagram (name,num_followers) VALUES (?,?)",(artist,num_followers))
-    conn.commit()
-
-def setUpStreamsTable(data, cur, conn):
-    for artist in data:
-        #num_streams = len(artist)
-        num_streams = get_monthly_listeners(artist, cur)
-        cur.execute("INSERT INTO Streams (name,num_streams) VALUES (?,?)",(artist,num_streams))
-    conn.commit()
-
-def gather_data(genre, genre_artists, cur, conn):
-    
-    setUpGenreTable(genre, genre_artists, cur, conn)
-    setUpIdTable(genre_artists, cur, conn)
-    setUpInstagramTable(genre_artists, cur, conn)
-    setUpStreamsTable(genre_artists, cur, conn)
